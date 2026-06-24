4adab3050ef5adc05bd84fe1ab4b40bd87dfeb97
Michael Herlea <m.herlea@student.maastrichtuniversity.nl>
2026-06-24 19:25:15 +0200

    Added the ability to load and delete reports from the database

--- database_connector.py
+++ database_connector.py
@@ -3,6 +3,7 @@ import sqlite3
 class DatabaseInitializer():
     def __init__(self):
         self.connection_obj = sqlite3.connect("database.db")
+        self.connection_obj.execute("PRAGMA foreign_keys = ON;")
         self.cursor_obj = self.connection_obj.cursor()
 
         create_application_table_query ="CREATE TABLE IF NOT EXISTS applications (" \
@@ -20,14 +21,15 @@ class DatabaseInitializer():
             "application_id INTEGER NOT NULL," \
             "time_spent INTEGER NOT NULL," \
             "PRIMARY KEY (report_id, application_id)," \
-            "FOREIGN KEY (report_id) REFERENCES reports(id)," \
-            "FOREIGN KEY (application_id) REFERENCES applications(id));"
+            "FOREIGN KEY (report_id) REFERENCES reports(id) ON DELETE CASCADE," \
+            "FOREIGN KEY (application_id) REFERENCES applications(id) ON DELETE CASCADE);"
         self.cursor_obj.execute(create_report_entries_table_query)
+        self.connection_obj.commit()
 
 class ReportDatabaseHandler():
-    def __init__(self):
-        self.connection_obj = sqlite3.connect("database.db")
-        self.cursor_obj = self.connection_obj.cursor()
+    def __init__(self, database_connection: DatabaseInitializer):
+        self.connection_obj = database_connection.connection_obj
+        self.cursor_obj = database_connection.cursor_obj
     
     def add_report(self, data):
         if not data:
@@ -50,3 +52,18 @@ class ReportDatabaseHandler():
             
             self.cursor_obj.execute(add_report_entries_query, (report_id, application_id, data[name]))
         self.connection_obj.commit()
+
+    def get_report_list(self):
+        get_report_list_query = "SELECT id, created_at FROM reports"
+        self.cursor_obj.execute(get_report_list_query)
+        return self.cursor_obj.fetchall()
+
+    def delete_report(self, entry_id):
+        delete_report_query = "DELETE FROM reports WHERE id = ?"
+        self.cursor_obj.execute(delete_report_query, (entry_id,))
+        self.connection_obj.commit()
+    
+    def get_report_contents(self, entry_id):
+        get_report_contents_query = "SELECT a.name, re.time_spent FROM report_entries re JOIN applications a ON re.application_id = a.id WHERE re.report_id = ?"
+        self.cursor_obj.execute(get_report_contents_query, (entry_id,))
+        return self.cursor_obj.fetchall()
--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QApplication
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
@@ -8,8 +9,13 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
+        temp_var = ["Report id: 1, Timestamp: 21min",
+            "Report id: 2, Timestamp: 24min",
+            "Report id: 3, Timestamp: 35min"]
+
         self.application_tracker = ApplicationTracker()
-        self.database_connector = DatabaseInitializer()
+        self.database_Initializer = DatabaseInitializer()
+        self.report_database_handler = ReportDatabaseHandler(self.database_Initializer)
 
         self.setWindowTitle("Focus tracker")
 
@@ -31,6 +37,9 @@ class MainWindow(QWidget):
         self.pie_chart = PieChartWidget()
         main_layout.addWidget(self.pie_chart)
 
+        self.report_list = ReportList(self.report_database_handler, self.pie_chart)
+        main_layout.addWidget(self.report_list)
+
         self.setLayout(main_layout)
     
     def on_start_button_clicked(self):
@@ -42,8 +51,71 @@ class MainWindow(QWidget):
         self.application_tracker.stop()
         self.status_indicator.set_color("red")
         self.pie_chart.update_chart(ApplicationData.get_data())
-        ReportDatabaseHandler().add_report(ApplicationData.get_data())
+        self.report_database_handler.add_report(ApplicationData.get_data())
+        self.report_list.update_list()
         ApplicationData.clear_data()
+
+class ReportList(QWidget):
+    def __init__(self, report_database_handler: ReportDatabaseHandler, pie_chart: PieChartWidget):
+        super().__init__()
+
+        self.report_database_handler = report_database_handler
+        self.list = None
+        self.pie_chart = pie_chart
+
+        self.main_layout = QVBoxLayout()
+        self.update_list()
+        self.setLayout(self.main_layout)
+
+    def update_list(self):
+        self.clear_list()
+        self.list = self.report_database_handler.get_report_list()
+        for entry in self.list:
+            self.main_layout.addWidget(ReportItem(entry, self))
+    
+    def clear_list(self):
+        while self.main_layout.count():
+            item = self.main_layout.takeAt(0)
+            widget = item.widget() # type: ignore
+            if widget is not None:
+                widget.deleteLater()
+
+    def delete_entry(self, entry_id):
+        self.report_database_handler.delete_report(entry_id)
+        self.update_list()
+    
+    def load_report(self, entry_id):
+        self.pie_chart.update_chart(dict(self.report_database_handler.get_report_contents(entry_id)))
+
+class ReportItem(QWidget):
+    def __init__(self, list, parent_list: ReportList):
+        super().__init__()
+
+        self.parent_list = parent_list
+
+        main_layout = QHBoxLayout()
+
+        main_layout.setContentsMargins(0, 0, 0, 0)
+        main_layout.setSpacing(5)
+
+        text = QLabel(f"ID: {list[0]}, timestamp: {list[1]}")
+        main_layout.addWidget(text)
+
+        open_button = QPushButton("Open")
+        open_button.clicked.connect(lambda: self.open_button_handler(list[0]))
+        main_layout.addWidget(open_button)
+
+        delete_button = QPushButton("Delete")
+        delete_button.clicked.connect(lambda: self.delete_button_handler(list[0]))
+        main_layout.addWidget(delete_button)
+
+        self.setLayout(main_layout)
+    
+    def open_button_handler(self, entry_id):
+        self.parent_list.load_report(entry_id)
+    
+    def delete_button_handler(self, entry_id):
+        self.parent_list.delete_entry(entry_id)
 
 class StatusIndicator(QLabel):
     def __init__(self):
